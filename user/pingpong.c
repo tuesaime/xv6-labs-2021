dc7d6f4c1951d2673b13ecd29b7ecd005b28720b
tuesaime <zsy18054799790@outlook.com>
2023-08-20 20:28:28 +0800

    first commit

---
+++ user/pingpong.c
@@ -0,0 +1,25 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char **argv) {
+	// 创建管道会得到一个长度为 2 的 int 数组
+	// 其中 0 为用于从管道读取数据的文件描述符，1 为用于向管道写入数据的文件描述符
+	int pp[2], pc[2];
+	pipe(pp); // 创建用于 父进程 -> 子进程 的管道
+	pipe(pc); // 创建用于 子进程 -> 父进程 的管道
+	
+	if(fork() != 0) { // parent process
+		write(pp[1], "!", 1); // 1. 父进程首先向发出该字节
+		char buf;
+		read(pc[0], &buf, 1); // 2. 父进程发送完成后，开始等待子进程的回复
+		printf("%d: received pong\n", getpid()); // 6. 父进程收到数据，输出 pong
+		wait(0);
+	} else { // child process
+		char buf;
+		read(pp[0], &buf, 1); // 3. 子进程读取管道，收到父进程发送的字节数据
+		printf("%d: received ping\n", getpid());// 4. 子进程收到数据，输出 ping
+		write(pc[1], &buf, 1); // 5. 子进程通过子->父管道，将字节送回父进程
+	}
+	exit(0);
+}
